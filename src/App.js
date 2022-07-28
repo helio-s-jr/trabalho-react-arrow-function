966228e0395e5f9957be0f26dac3e705f83daf82
Helio Junior <heliojr.zpt@gmail.com>
2022-07-28 17:38:41 -0300

    Trabalho de arrow function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+  
+  ARROWFUNCTION: () => {}
+
+  mensagem = () => {
+    return <h2>Passando mensagem pela função</h2>
+  }
+
+  temosMensagem = (msg) =>{
+    return <h2>{msg}</h2>
+  }
+
+  dobro = (numero) =>{
+    return <h2>O dobro do número: {numero*2}</h2>
+  }
+
+  numero = (num) => {
+    if(num >= 0){
+      return <h3>Número positivo</h3>
+    }else{
+      return <h3>Número negativo</h3>
+    }
+  }
 }
-
-export default App;
